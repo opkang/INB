4e95d4df9541668323184c14741cb9e7c315350d
cheah wooi kang <cheahwooikang02@gmail.com>
2022-03-28 17:36:14 +0800

    commit

--- user-service/src/main/java/userService/UserServiceAspect.java
+++ user-service/src/main/java/userService/UserServiceAspect.java
@@ -1,7 +1,9 @@
 package userService;
 
-
 import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.AfterThrowing;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.springframework.stereotype.Component;
@@ -14,12 +16,39 @@ import userService.entity.User;
 public class UserServiceAspect {
 
     //user(userID,firstName,lastName,email)
-    @Before("execution(* userService.controller.UserController.saveUser(User)), && args(user)")
+ /*   @Before("execution(* userService.controller.UserController.saveUser(..)) && args(user)")
     public void beforeAddingUser(JoinPoint jp,User user){
         log.info("User: " + user);
         log.info("Before adding");
+    }*/
+
+    @AfterThrowing(value = "execution(* userService.*.*.*(..))", throwing = "error")
+    public void afterThrowingAdvice(JoinPoint jp, Throwable error){
+        System.out.println("Method Signature: "  + jp.getSignature());
+        System.out.println("Exception: "+error);
     }
 
+    @Around("execution(* userService.controller.UserController.saveUser(..)) && args(user)")
+    public void beforeAddingUser(ProceedingJoinPoint pjp, User user){
+        try {
+            if(validateUser(user)){
+                log.info("User: " + user);
+                log.info("Before adding");
+                pjp.proceed();
+            }
+        }catch (Exception e) {
+            log.info("Adding user Failed...");
+            log.info("User: " + user);
+            log.error("Reason : " + e.getMessage());
+            log.error("Exception Detail : " + e.getLocalizedMessage());
+        } catch (Throwable e) {
+            e.printStackTrace();
+        }
 
-
+    }
+    private boolean validateUser(User user){
+        return !(user.getFirstName() == null || user.getFirstName().isEmpty() &&
+                user.getLastName() == null || user.getLastName().isEmpty() &&
+                user.getEmail() == null || user.getEmail().isEmpty());
+    }
 }
